5549c49ada1a3755359b0e82314eaa098d1b4e51
Dogukan Yakupoglu <dogukan.yakupoglu@gmail.com>
2023-07-29 02:38:12 +0300

    performance is increased

--- js/map.js
+++ js/map.js
@@ -1,79 +1,171 @@
 /// Global scope variables.
+let currentCity = "";
+let dropdownSelection = $("#adminPanelDropdown:first").val();
+let fetchedData = {}; /// Real data that will come from backend.
 const dummyData = {
-  cities: [
-    {
-      id: "ankara",
+  cities: {
+    ankara: {
       color: "ff7800",
-      parties: [
-        {
+      parties: {
+        akp: {
+          partyAcronym: "AKP",
           partyName: "ak parti",
-          partyPercantage: 31.69,
-          totalVote: "31M",
+          partyPercantage: 10.1,
+          totalVote: 12000,
           color: "ff7800",
         },
-        {
+        chp: {
+          partyAcronym: "CHP",
           partyName: "chp",
-          partyPercantage: 31.69,
-          totalVote: "69M",
+          partyPercantage: 10.1,
+          totalVote: 25000,
           color: "a90000",
         },
-        {
+        iyi: {
+          partyAcronym: "İYİ",
           partyName: "iyi parti",
-          partyPercantage: 31.69,
-          totalVote: "10M",
+          partyPercantage: 10.1,
+          totalVote: 10000,
           color: "0f94ca",
         },
-      ],
+      },
     },
-    {
-      id: "istanbul",
+    istanbul: {
       color: "ff7800",
-      parties: [
-        {
+      parties: {
+        akp: {
+          partyAcronym: "AKP",
           partyName: "ak parti",
-          partyPercantage: 31.69,
-          totalVote: "31M",
+          partyPercantage: 10.1,
+          totalVote: 12000,
           color: "ff7800",
         },
-        {
+        chp: {
+          partyAcronym: "CHP",
           partyName: "chp",
-          partyPercantage: 31.69,
-          totalVote: "69M",
+          partyPercantage: 10.1,
+          totalVote: 25000,
           color: "a90000",
         },
-      ],
+      },
     },
-    {
-      id: "van",
+    van: {
       color: "ff7800",
-      parties: [
-        {
+      parties: {
+        akp: {
+          partyAcronym: "AKP",
           partyName: "ak parti",
-          partyPercantage: 31.69,
-          totalVote: "31M",
+          partyPercantage: 10.1,
+          totalVote: 12000,
           color: "ff7800",
         },
-        {
+        chp: {
+          partyAcronym: "CHP",
           partyName: "chp",
-          partyPercantage: 31.69,
-          totalVote: "69M",
+          partyPercantage: 10.1,
+          totalVote: 25000,
           color: "a90000",
         },
-        {
+        iyi: {
+          partyAcronym: "İYİ",
           partyName: "iyi parti",
-          partyPercantage: 31.69,
-          totalVote: "10M",
+          partyPercantage: 10.1,
+          totalVote: 10000,
           color: "0f94ca",
         },
-        {
-          partyName: "alfa parti",
-          partyPercantage: 69.31,
-          totalVote: "3169M",
+        mhp: {
+          partyAcronym: "MHP",
+          partyName: "MHP",
+          partyPercantage: 45.41,
+          totalVote: 25000,
           color: "ff00ff",
         },
-      ],
+      },
     },
-  ],
+  },
+  // cities: [
+  // {
+  //   id: "ankara",
+  //   color: "ff7800",
+  //   parties: [
+  //     {
+  //       partyAcronym: "AKP",s
+  //       partyName: "ak parti",
+  //       partyPercantage: 10.1,
+  //       totalVote: 12000,
+  //       color: "ff7800",
+  //     },
+  //     {
+  //       partyAcronym: "CHP",
+  //       partyName: "chp",
+  //       partyPercantage: 10.1,
+  //       totalVote: 25000,
+  //       color: "a90000",
+  //     },
+  //     {
+  //       partyAcronym: "İYİ",
+  //       partyName: "iyi parti",
+  //       partyPercantage: 10.1,
+  //       totalVote: 10000,
+  //       color: "0f94ca",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: "istanbul",
+  //   color: "ff7800",
+  //   parties: [
+  //     {
+  //       partyAcronym: "AKP",
+  //       partyName: "ak parti",
+  //       partyPercantage: 10.1,
+  //       totalVote: 12000,
+  //       color: "ff7800",
+  //     },
+  //     {
+  //       partyAcronym: "CHP",
+  //       partyName: "chp",
+  //       partyPercantage: 10.1,
+  //       totalVote: 25000,
+  //       color: "a90000",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: "van",
+  //   color: "ff7800",
+  //   parties: [
+  //     {
+  //       partyAcronym: "AKP",
+  //       partyName: "ak parti",
+  //       partyPercantage: 10.1,
+  //       totalVote: 12000,
+  //       color: "ff7800",
+  //     },
+  //     {
+  //       partyAcronym: "CHP",
+  //       partyName: "chp",
+  //       partyPercantage: 10.1,
+  //       totalVote: 25000,
+  //       color: "a90000",
+  //     },
+  //     {
+  //       partyAcronym: "İYİ",
+  //       partyName: "iyi parti",
+  //       partyPercantage: 10.1,
+  //       totalVote: 10000,
+  //       color: "0f94ca",
+  //     },
+  //     {
+  //       partyAcronym: "MHP",
+  //       partyName: "MHP",
+  //       partyPercantage: 45.41,
+  //       totalVote: 25000,
+  //       color: "ff00ff",
+  //     },
+  //   ],
+  // },
+  // ],
   patyPercentages: [
     {
       id: "ak parti",
@@ -99,11 +191,11 @@ const dummyData = {
     usedVote: "201.807",
     validVote: "201.807",
   },
+  totalNumberOfParties: ["AKP", "CHP", "İYİ", "MHP"],
 };
 
-const apiUrl = "./dummyData.JSON";
-const exampleURL = "https://turkeyMap/admin/data";
-const isAdmin = exampleURL.split("/")[3] === "admin" ? true : false;
+const apiUrl = "https://turkeyMap/admin/data"; /// It can be changed according to the updated data url.
+const isAdmin = apiUrl.split("/")[3] === "admin" ? true : false;
 
 /// Function to fetch data from the backend for users.
 async function fetchData() {
@@ -115,6 +207,7 @@ async function fetchData() {
 
     // Assuming your backend returns data in JSON format
     const data = await response.json();
+    fetchedData = data;
 
     // Display the data for users
     console.log("Data for users:", data);
@@ -126,13 +219,17 @@ async function fetchData() {
 /// Function to send data to the backend for admins.
 async function sendDataToBackend(data) {
   try {
-    const response = await fetch(`${apiUrl}`, {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
+    const response = await fetch(
+      `${apiUrl}/cities/${currentCity}/parties/${dropdownSelection}/totalvote`,
+      {
+        /// It can be changed according to the updated data url.
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
 
     if (!response.ok) {
       throw new Error("Network response was not ok");
@@ -140,6 +237,7 @@ async function sendDataToBackend(data) {
 
     // Assuming your backend returns a response (e.g., success or updated data)
     const responseData = await response.json();
+    fetchedData = responseData;
 
     // Display the response data for admins
     console.log("Response from server for admin:", responseData);
@@ -150,9 +248,13 @@ async function sendDataToBackend(data) {
 
 /// Bind the map container events and some CSS features.
 function bindActionEvents() {
-  $("#submitAdminDataButton")
-    .off()
-    .on("click", onSubmitAdminData);
+  if (isAdmin) {
+    $(".button-class").off().on("click", onClickCity);
+  } else {
+    $(".button-class").off();
+  }
+
+  $("#submitAdminDataButton").off().on("click", onSubmitAdminData);
 
   $(".path-class")
     .off()
@@ -165,23 +267,67 @@ function bindActionEvents() {
       onMoveMovableContainer(e);
     });
 
+  $("#adminPanelInput").off().on("input", accesOnlyInteger);
+
+  function accesOnlyInteger(e) {
+    $(this).val(
+      $(this)
+        .val()
+        .replace(/[^0-9]/g, "")
+    );
+  }
+
+  function onClickCity(e) {
+    const cityName = e.currentTarget.id;
+    currentCity = cityName;
+    $("#adminPanelCurrentCity").html(
+      cityName.charAt(0).toUpperCase() + cityName.slice(1)
+    );
+
+    disableAdminPanel(false);
+  }
+
   function onSubmitAdminData(e) {
-    const dataToSendByAdmin = {};
-    sendDataToBackend(dataToSendByAdmin);
+    let dataToSendByAdmin = 0;
+    let inputValue = Number($("#adminPanelInput").val()) || 0;
+    dropdownSelection =
+      $("#adminPanelDropdown").val() || $("#adminPanelDropdown:first").val();
+
+    dummyData.Object.keys(cities).forEach((city, index) => {
+      if (city.id === currentCity) {
+        city.parties.forEach((party, index) => {
+          if (party.partyAcronym === dropdownSelection) {
+            dataToSendByAdmin = party.totalVote + inputValue; /// Change selected data on a variable.
+          }
+        });
+      }
+    });
+
+    if (
+      inputValue !== "" &&
+      inputValue !== 0 &&
+      inputValue !== undefined &&
+      inputValue !== null
+    ) {
+      sendDataToBackend(dataToSendByAdmin); /// Send changed data to the backend.
+    } else {
+      alert("You should input your data correctly!");
+    }
+    // disableAdminPanel(true);
   }
 
   function onMouseoverCity(e) {
-    const cityName = $(e.currentTarget)
-      .parent()
-      .attr("id");
+    const cityName = $(e.currentTarget).parent().attr("id");
 
     /// Set current city name.
     $(".movable-container-city-name").html(cityName.toUpperCase()); // cityName.charAt(0).toUpperCase() + cityName.slice(1)
 
-    /// Find city data when on over than call function.
-    dummyData.cities.forEach((item, index) => {
-      if (item.id === cityName) {
-        setCityData(item);
+    /// Find city data when mouse on over it.
+
+    let tempData = dummyData.cities;
+    Object.keys(tempData).forEach((item, index) => {
+      if (item === cityName) {
+        setCityData(tempData[item]);
       }
     });
 
@@ -198,6 +344,8 @@ function bindActionEvents() {
   }
 
   const onMoveMovableContainer = (e) => {
+    let windowWidth = $(document).width();
+
     $(".movable-container").show();
     //Try, catch to avoid any errors for touch screens (Error thrown when user doesn't move his finger)
     try {
@@ -265,11 +413,58 @@ function setCityData(data) {
   const parentDom = $(".movable-container-parties-container");
 
   /// Remove created uniques city data html elements.
-  $(".movable-container-parties-wrapper")
-    .empty()
-    .remove();
+  $(".movable-container-parties-wrapper").empty().remove();
 
-  data.parties.forEach((item, index) => {
+  let tempData = data.parties;
+  Object.keys(tempData).forEach((item, index) => {
+    console.info(tempData[item]);
+    const newPartyDOM = $("<div></div>").addClass(
+      "movable-container-parties-wrapper"
+    );
+    parentDom.append(newPartyDOM);
+
+    const newChild_1 = $("<div></div>").addClass(
+      "movable-container-child-containers"
+    );
+    newPartyDOM.append(newChild_1);
+    const newChildChild_1 = $("<div></div>")
+      .addClass("movable-container-partyName movable-container-fontWeight")
+      .html(tempData[item].partyName);
+    newChild_1.append(newChildChild_1);
+
+    const newChild_2 = $("<div></div>").addClass(
+      "movable-container-child-containers"
+    );
+    newPartyDOM.append(newChild_2);
+    const newChildChild_2 = $("<div></div>")
+      .addClass(
+        "movable-container-partyPercantage movable-container-fontSize movable-container-fontWeight"
+      )
+      .css("color", "#" + tempData[item].color)
+      .html("%" + tempData[item].partyPercantage);
+    newChild_2.append(newChildChild_2);
+
+    const newChild_3 = $("<div></div>").addClass(
+      "movable-container-child-containers"
+    );
+    newPartyDOM.append(newChild_3);
+    const newChildChild_3 = $("<div></div>")
+      .addClass(
+        "movable-container-partyNumberOfVote movable-container-fontSize movable-container-fontWeight"
+      )
+      .css("color", "#" + tempData[item].color)
+      .html(tempData[item].totalVote);
+    newChild_3.append(newChildChild_3);
+  });
+}
+
+function setPartiesTotheDropdown(data) {
+  const parentDom = $("#adminPanelDropdown");
+
+  /// Remove created uniques city data html elements.
+  $(".movable-container-parties-wrapper").empty().remove();
+
+  data.forEach((item, index) => {
     const newPartyDOM = $("<div></div>").addClass(
       "movable-container-parties-wrapper"
     );
@@ -283,43 +478,34 @@ function setCityData(data) {
       .addClass("movable-container-partyName movable-container-fontWeight")
       .html(item.partyName);
     newChild_1.append(newChildChild_1);
-
-    const newChild_2 = $("<div></div>").addClass(
-      "movable-container-child-containers"
-    );
-    newPartyDOM.append(newChild_2);
-    const newChildChild_2 = $("<div></div>")
-      .addClass(
-        "movable-container-partyPercantage movable-container-fontSize movable-container-fontWeight"
-      )
-      .css("color", "#" + item.color)
-      .html("%" + item.partyPercantage);
-    newChild_2.append(newChildChild_2);
-
-    const newChild_3 = $("<div></div>").addClass(
-      "movable-container-child-containers"
-    );
-    newPartyDOM.append(newChild_3);
-    const newChildChild_3 = $("<div></div>")
-      .addClass(
-        "movable-container-partyNumberOfVote movable-container-fontSize movable-container-fontWeight"
-      )
-      .css("color", "#" + item.color)
-      .html(item.totalVote);
-    newChild_3.append(newChildChild_3);
   });
+}
+
+/// Util functions.
+function disableAdminPanel(state) {
+  if (state) {
+    $("#adminPanelInput").attr("disabled", true);
+    $("#adminPanelDropdown").attr("disabled", true);
+    $("#submitAdminDataButton").attr("disabled", true);
+  } else {
+    $("#adminPanelInput").attr("disabled", false);
+    $("#adminPanelDropdown").attr("disabled", false);
+    $("#submitAdminDataButton").attr("disabled", false);
+  }
 }
 
 /// Function for wrap all app. functions.
 function initializeApplication() {
-  if (!isAdmin) {
-    $("#adminPanel").hide();
-  }
-
   fetchData();
   bindActionEvents();
   setPartyPercentagesData(dummyData.patyPercentages);
   setGeneralInformationData(dummyData.generalInformation);
+  // setPartiesTotheDropdown(dummyData.allNameOfParties);
+
+  if (!isAdmin) {
+    $("#adminPanel").hide();
+    $(".button-class").off().on("click", onClickCity);
+  }
 }
 
 initializeApplication();
